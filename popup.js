d004c16f90c2c2368c32c8b50e10afd7024a0bfb
hvd_007 <harshdixit1981@gmail.com>
2022-07-04 16:58:24 +0530

    second

--- popup.js
+++ popup.js
@@ -5,11 +5,13 @@ $(function(){
         arr = result.arr;
         $.map(arr,(e,i)=>{
           let x = $(`#para`).html();
-          let y = `<p id="para${i}">${e}</p>`;
+          let y = `<p id="para${i}">${e}</p>
+          <button class="btn" id="${i}">Delete</button>`;
           $('#para').html(y + x);
      })
     });
-    $("#btn").click(function(){
+    
+    $("#btn-delete-all").click(function(){
       chrome.storage.local.set({'arr':['']}, function() {
         console.log('Value is set ');
       })
@@ -19,4 +21,26 @@ $(function(){
         $('#para').text(result.arr[0]);
       })
     });
-})
+
+    $(document).on('click','.btn',function(){
+
+      console.log($(this).attr("id"));
+      var val = $(this).attr("id");
+      chrome.storage.local.get(['arr'], function(result) {
+        var arr = result.arr;
+        console.log(arr);
+        arr[val] = '';
+        console.log(arr);
+        chrome.storage.local.set({'arr':arr}, function() {
+          console.log('text is get deleted ');
+        })
+     })
+    })
+    //val() - Sets or returns the value of form field
+   
+     
+    // chrome.storage.local.get(['btn'],function(result){
+    //   console.log(result.btn);
+    // })
+}) 
+
